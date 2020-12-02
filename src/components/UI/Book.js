b3e52d230c8b791925826268d2a9cfb06ebfe87b
Daniel Viramontes <danielviramo@gmail.com>
2020-12-02 15:38:33 -0600

    Book.js

--- src/components/UI/Book.js
+++ src/components/UI/Book.js
@@ -1,42 +1,42 @@
 import React from "react";
 
 const Book = (props) => {
-    const onSavedBookClick = () => {
-        const apiURL = "http://localhost:4000/api/v1/saves";
+  const onSaveBookClick = () => {
+    const apiURL = "http://localhost:4000/api/v1/saves";
 
-        const saveDate = {
-            bookId: props.id, 
-        };
-
-        const reqObj = {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                accepts: "application/json",
-            },
-            body: JSON.stringify(saveData),
-        };
-
-        fetch(apiURL,reqObj)
-        .then((resp) => resp.json())
-        .then((save) => {
-            console.log(save);
-        })
-        .catch((err) => console.log(err));
+    const saveData = {
+      bookId: props.id,
     };
 
-    return (
-        <div style={{marginTop: 20, padding:10,border: "2px solid #000"}}>
-            <img src={props.image} alt="Book" />
-            <h2>{props.title}</h2>
-            <h3>{props.subtitle}</h3>
-            <p>{props.description}</p>
-            {props.author.legth === 0
-            ? null 
-            : props.author.map((author,index) => <p key={index}>{author}</p>)}
-            <button onClick={onSavedBookClick}>Save</button>
-        </div>
-    );
+    const reqObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        accepts: "application/json",
+      },
+      body: JSON.stringify(saveData),
+    };
+
+    fetch(apiURL, reqObj)
+      .then((resp) => resp.json())
+      .then((save) => {
+        console.log(save);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div style={{ marginTop: 20, padding: 10, border: "2px solid #000" }}>
+      <img src={props.image} alt="Book" />
+      <h2>{props.title}</h2>
+      <h3>{props.subtitle}</h3>
+      <p>{props.description}</p>
+      {props.authors.length === 0
+        ? null
+        : props.authors.map((author, index) => <p key={index}>{author}</p>)}
+      <button onClick={onSaveBookClick}>Save</button>
+    </div>
+  );
 };
 
 export default Book;
---
+++ src/components/UI/SavedBook.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+const SavedBook = (props) => {
+  const [image, setImage] = useState(null);
+  const [title, setTitle] = useState("");
+  const [subtitle, setSubtitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [authors, setAuthors] = useState([]);
+
+  
+}
