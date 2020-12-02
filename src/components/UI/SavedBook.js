ca306d62ab81980e958e77d78b448a88173a5d89
Daniel Viramontes <danielviramo@gmail.com>
2020-12-02 15:40:00 -0600

    Book.js

--- src/components/UI/SavedBook.js
+++ src/components/UI/SavedBook.js
@@ -7,5 +7,37 @@ const SavedBook = (props) => {
   const [description, setDescription] = useState("");
   const [authors, setAuthors] = useState([]);
 
-  
-}
+  useEffect(() => {
+    const googleApiURL = `https://www.googleapis.com/books/v1/volumes/${props.bookId}`;
+
+    fetch(googleApiURL)
+      .then((resp) => resp.json())
+      .then((book) => {
+        if (!book) return;
+        if (book.volumeInfo.imageLinks) {
+          setImage(book.volumeInfo.imageLinks.smallThumbnail);
+        }
+        setTitle(book.volumeInfo.title);
+        setSubtitle(book.volumeInfo.subtitle);
+        setDescription(book.volumeInfo.description);
+        if (book.volumeInfo.authors) {
+          setAuthors(book.volumeInfo.authors);
+        }
+      })
+      .catch((err) => console.log(err));
+  }, [props.bookId]);
+
+  return (
+    <div style={{ marginTop: 20, padding: 10, border: "2px solid #000" }}>
+      <img src={image} alt="Book" />
+      <h2>{title}</h2>
+      <h3>{subtitle}</h3>
+      <p>{description}</p>
+      {authors.length === 0
+        ? null
+        : authors.map((author, index) => <p key={index}>{author}</p>)}
+    </div>
+  );
+};
+
+export default SavedBook;
